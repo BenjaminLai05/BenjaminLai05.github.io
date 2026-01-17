909974874a54bcce4f1d4c27322e12c119f28735
BenjaminLai05 <a1888072@adelaide.edu.au>
2026-01-17 17:00:10 +1030

    Fix database connection protocol and password for local setup and deployment

--- WDC_Proj/app.js
+++ WDC_Proj/app.js
@@ -21,7 +21,7 @@ var dbConnectionPool = mysql.createPool({
     host: process.env.DB_HOST || '127.0.0.1',
     port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
+    password: process.env.DB_PASSWORD || 'admin123',
     database: process.env.DB_NAME || 'webapp_database',
 });
 

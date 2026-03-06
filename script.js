22549c5a61c2cf905750d1b3a47c0fa24c640d53
BenjaminLai05 <a1888072@adelaide.edu.au>
2026-03-06 18:03:32 +1030

    bruh
    a

--- script.js
+++ script.js
@@ -336,7 +336,7 @@ function initDynamicDownloads() {
 
     // Check if user is on Mac
     if (userAgent.includes('mac')) {
-        downloadBtn.href = "https://github.com/BenjaminLai05/nba_prediction_algo/releases/download/1/NBA_Elo_CLI_Mac.zip";
+        downloadBtn.href = "https://github.com/BenjaminLai05/nba_prediction_algo/releases/download/v1.1/NBA_Elo_CLI_Mac.zip";
         // Note: Text remains unchanged as requested
     }
 }
